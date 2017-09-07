c42cda8e9745eaf8e8e5afb89a411ce945f492a2
Elgin Davis <elgindavis@college.harvard.edu>
2017-09-06 22:01:10 -0700

    wednesday work

--- pset1/m61.c
+++ pset1/m61.c
@@ -12,12 +12,31 @@
 ///    either return NULL or a unique, newly-allocated pointer value.
 ///    The allocation request was at location `file`:`line`.
 
+int bytes = 0;
+int allocations = 0;
+int fails = 0;
+
 void* m61_malloc(size_t sz, const char* file, int line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
-    // Your code here.
-    return base_malloc(sz);
+    
+    // return request located at file: line 
+    if (!malloc(sz)){
+    	fails += 1;
+    	abort();
+    }
+   
+    else {
+    
+    allocations += 1;
+    bytes += sz;
+    	malloc(sz);
+    }
+    
+    //printf("Pointer name: %s, file name:%s, line number:%s \n", ptr, file, line);
+    
+    
+   
 }
-
 
 /// m61_free(ptr, file, line)
 ///    Free the memory space pointed to by `ptr`, which must have been
@@ -28,9 +47,20 @@ void* m61_malloc(size_t sz, const char* file, int line) {
 void m61_free(void *ptr, const char *file, int line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
     // Your code here.
-    base_free(ptr);
+    if(ptr == NULL){
+        ptr;
+    }
+    
+    else {
+    free(ptr);
+    bytes -= sizeof(*ptr);
+    allocations -= 1;
+        
+    }
+    printf("The free was called at location- %s : %d", file, line );
+    
+    
 }
-
 
 /// m61_realloc(ptr, sz, file, line)
 ///    Reallocate the dynamic memory pointed to by `ptr` to hold at least
@@ -53,7 +83,6 @@ void* m61_realloc(void* ptr, size_t sz, const char* file, int line) {
     return new_ptr;
 }
 
-
 /// m61_calloc(nmemb, sz, file, line)
 ///    Return a pointer to newly-allocated dynamic memory big enough to
 ///    hold an array of `nmemb` elements of `sz` bytes each. The memory
@@ -70,7 +99,6 @@ void* m61_calloc(size_t nmemb, size_t sz, const char* file, int line) {
     return ptr;
 }
 
-
 /// m61_getstatistics(stats)
 ///    Store the current memory statistics in `*stats`.
 
@@ -78,8 +106,19 @@ void m61_getstatistics(struct m61_statistics* stats) {
     // Stub: set all statistics to enormous numbers
     memset(stats, 255, sizeof(struct m61_statistics));
     // Your code here.
-}
 
+    stats->nactive = allocations;
+    stats->ntotal = bytes;
+    
+    // unsigned long long nactive;           // number of active allocations [#malloc - #free]
+    // unsigned long long active_size;       // number of bytes in active allocations
+    // unsigned long long ntotal;            // number of allocations, total
+    // unsigned long long total_size;        // number of bytes in allocations, total
+    // unsigned long long nfail;             // number of failed allocation attempts
+    // unsigned long long fail_size;         // number of bytes in failed allocation attempts
+    // char* heap_min;                       // smallest address in any region ever allocated
+    // char* heap_max;                       // largest address in any region ever allocated
+}
 
 /// m61_printstatistics()
 ///    Print the current memory statistics.
@@ -87,7 +126,9 @@ void m61_getstatistics(struct m61_statistics* stats) {
 void m61_printstatistics(void) {
     struct m61_statistics stats;
     m61_getstatistics(&stats);
-
+	stats.nactive = allocations;
+	stats.ntotal = bytes;
+	stats.nfail = 0;
     printf("malloc count: active %10llu   total %10llu   fail %10llu\n",
            stats.nactive, stats.ntotal, stats.nfail);
     printf("malloc size:  active %10llu   total %10llu   fail %10llu\n",
