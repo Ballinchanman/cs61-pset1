26d7169ce0505605637ed1ffa6a928ccd76dcd88
Chandler Brown <cbrown02@college.harvard.edu>
2017-09-07 22:45:25 -0400

    heap min/max first try

--- pset1/m61.c
+++ pset1/m61.c
@@ -26,6 +26,12 @@ int freed_bytes = 0;
 //number of bytes failed
 int fail_bytes = 0;
 
+//minimum address allocated
+char* min_adrs = NULL;
+
+//maximum address allocated
+char* max_adrs = NULL;
+
 /// m61_malloc(sz, file, line)
 ///    Return a pointer to `sz` bytes of newly-allocated dynamic memory.
 ///    The memory is not initialized. If `sz == 0`, then m61_malloc may
@@ -46,7 +52,19 @@ void* m61_malloc(size_t sz, const char* file, int line) {
     
     	num_allocations += 1;
     	alloc_bytes += sz;
-    	return malloc(sizeof(sz));
+        void* mal = malloc(sizeof(sz));
+        if (min_adrs == NULL) {
+            min_adrs = mal;
+            max_adrs = mal;
+        }
+        else if (mal - min_adrs < 0) {
+            min_adrs = mal;
+        }
+        else if (max_adrs - mal < 0) {
+            max_adrs = mal;
+        }
+        
+    	return mal;
     }
     
     //printf("Pointer name: %s, file name:%s, line number:%s \n", ptr, file, line);
@@ -139,7 +157,9 @@ void m61_getstatistics(struct m61_statistics* stats) {
 
     stats->fail_size = fail_bytes;
 	
-    // stats->heap_min = 
+    stats->heap_min = min_adrs;
+    
+    stats->heap_max = max_adrs;
     
     // unsigned long long nactive;           // number of active allocations [#malloc - #free]
     // unsigned long long active_size;       // number of bytes in active allocations
